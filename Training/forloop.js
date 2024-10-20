06fcc92062cc1ad8ebf131c453a7b07e9562387a
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-20 11:12:26 +0600

    print 5 time

---
+++ Training/forloop.js
@@ -0,0 +1,10 @@
+var result=0;
+for(var x=1; x<=5;x++)
+{
+    var num1= parseInt(prompt("Enter first number:"));
+    var num2= parseInt(prompt("Enter Second number:"));
+     result=num1+ num2;
+     console.log("sum of number" + result);
+    
+}
+
