7071dc7c3e0f6e460ba17f821ebdb1e555828f54
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-20 10:09:02 +0600

    first commit

---
+++ Training/if else.js
@@ -0,0 +1,9 @@
+var marks = prompt("Enter your marks: ");
+
+if (marks >= 80 && marks <= 100) console.log("A+");
+else if (marks >= 70 && marks <= 79) console.log("A");
+else if (marks >= 60 && marks <= 69) console.log("A-");
+else if (marks >= 50 && marks <= 59) console.log("B");
+else if (marks >= 40 && marks <= 49) console.log("C");
+else if (marks >= 33 && marks <= 39) console.log("D");
+else console.log("fail");
