8de8a16d151ce4ab8cde21b5c2a69012916b8493
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-21 20:51:47 +0600

    number_method

---
+++ practise/number_method.js
@@ -0,0 +1,11 @@
+var num = "20";
+
+console.log(typeof num);
+num1 = parseInt(num);
+console.log(typeof num1);
+console.log(typeof num);
+
+var num2 = 20;
+num3 = parseFloat(num2);
+console.log(typeof num3);
+console.log(typeof num2);
