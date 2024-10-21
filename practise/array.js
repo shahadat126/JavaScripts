07f75421705a5f17a5bac46f411cfa8dc431542d
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-21 21:27:09 +0600

    array concat

--- practise/array.js
+++ practise/array.js
@@ -3,10 +3,23 @@ names[0]="sabil";
 names[1]="nabil";
 names[2]="kabil";
 names[3]="habil";
+names[4]="tabil"
 
 console.log(names[2]);
-console.log(names.length);
+// console.log(names.length);
 
-var name= ["sabil","kabil","nabil","habil"];
-console.log(name[1]);
+var f_name= ["sabil","kabil","nabil","habil","ahad"];
+
+console.log(f_name.length);
+console.log(f_name[4]);
+f_name.push("shimul");
+console.log(f_name.length);
+f_name.pop();
+console.log(f_name);
+
+//concatination array
+var l_name =["khan","sikdar","sheikh","hossain"];
+var full_name = f_name.concat(l_name);
+console.log(full_name);
+
 
