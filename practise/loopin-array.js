c3c04685e3aa2946015c8d4a16a351f814676f98
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-21 21:44:22 +0600

    loop-in-array

---
+++ practise/loopin-array.js
@@ -0,0 +1,10 @@
+var num = new Array();
+for (var i = 0; i < 5; i++) {
+  num[i] = parseInt(prompt("Enter a number"));
+}
+var sum = 0;
+
+for (var i = 0; i < 5; i++) {
+  sum = sum + num[i];
+}
+console.log(sum);
