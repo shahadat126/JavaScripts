80a711e63de14813044b95affffb40c5f8e1e636
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-21 08:53:19 +0600

    function

---
+++ practise/function.js
@@ -0,0 +1,9 @@
+function Sum(x,y){
+    var addition = x + y;
+    document.write("sum= " +addition);
+}
+
+var x = parseInt(prompt("enter a number"));
+var y = parseInt(prompt("enter a number"));
+
+Sum(x,y);
