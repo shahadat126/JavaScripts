69b04d84fb0c9ad239acb334b1d4ee3567d2cab3
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-22 16:03:05 +0600

    date-objects

---
+++ practise/date-object.js
@@ -0,0 +1,21 @@
+var date = new Date();
+console.log(date);
+
+var year = date.getFullYear();
+console.log(year);
+
+var month = date.getMonth();
+console.log(month);
+
+var currentdate = date.getDate()
+console.log(currentdate);
+
+var currentday = date.getDay()
+console.log(currentday);
+
+var currentHour = date.getHours()
+console.log(currentHour);
+
+var currentMinute = date.getMinutes()
+console.log(currentMinute);
+
