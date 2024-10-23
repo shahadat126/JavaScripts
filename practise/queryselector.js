8012762311eb9ce8d63758a97ca5104e680a6d2a
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-23 12:50:44 +0600

    Query selector

---
+++ practise/queryselector.js
@@ -0,0 +1,14 @@
+var heading2 = document.querySelector("#heading1");
+heading2.innerHTML = "change the heading 2";
+
+
+var heading1 = document.querySelector(".heading2");
+heading1.innerHTML = "change the heading 1";
+
+var linkselect = document.querySelector("a");
+linkselect.innerHTML = "sk";
+var linkselect = document.querySelector(".my-div a");
+linkselect.innerHTML = "facebook";
+
+var parasel = document.querySelectorAll("p")[1];
+parasel.innerHTML = "this is text";
