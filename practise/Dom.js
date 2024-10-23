3b28f7ea55b35ce4c1be7f5effb26930f6563fc4
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-23 07:10:53 +0600

    DOM

---
+++ practise/Dom.js
@@ -0,0 +1,20 @@
+//getelementby id
+
+// document.getElementById("heading").innerHTML = "Hello";
+
+//using varaiable
+// var pa = document.getElementById("para");
+// pa.innerHTML = "I am shahadat ";
+
+//getelement by class also include index although differnt class name
+var heading2 = document.getElementsByClassName("heading2")[0];
+heading2.innerHTML = "change the heading 2";
+
+// var heading2 = document.getElementsByClassName("heading2")[1];
+// heading2.innerHTML = "change the heading 2";
+
+//getelement by tagname
+//  document.getElementsByTagName("h1")[0].innerHTML = "shimul";
+
+
+
