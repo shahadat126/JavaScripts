0f1423d9f741d32c2ba48e7ea6ac168c6e249353
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-26 18:54:42 +0600

    Event Listenar

---
+++ DOM/event_listenar.js
@@ -0,0 +1,11 @@
+document.querySelector("button").addEventListener("click", my_function);
+
+function my_function() {
+  alert("hello");
+}
+
+
+// document.querySelector("button").addEventListener("click", function my_function() {
+//     alert("hello");
+//   });
+
