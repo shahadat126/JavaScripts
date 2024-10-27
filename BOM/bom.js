0e5b6c82d324d6146ad07993c46146c3d7ab079a
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-27 14:22:01 +0600

    BOM| Location object

---
+++ BOM/bom.js
@@ -0,0 +1,16 @@
+//BOM(Browser object model)
+//window object
+//location object
+console.log(window.document);
+console.log(window.location);
+//href
+console.log(location.href);
+//protocol
+console.log("protocol= "+location.protocol);
+//hostname
+
+console.log("hostname= "+location.hostname);
+// port
+console.log("port= "+location.port);
+//pathname
+console.log("pathname= "+location.pathname);
