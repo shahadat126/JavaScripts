3567520c9ab3a7c42b8127084a5d7a5459f1802d
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-28 19:05:19 +0600

    Variables and Function

--- ES6/variables.js
+++ ES6/variables.js
@@ -5,3 +5,16 @@
 // console.log(x);
 //output will be 6
 
+//let is used for local variable
+
+let x = 10;
+let y=25;
+let sum= x+y;
+ document.write(`sum is ${sum} .<br> `);
+
+ //function
+ const add =(x,y) =>{
+    let sum = x+ y;
+    document.write(`sum is = ${sum}`);
+ }
+ add(10,20);
