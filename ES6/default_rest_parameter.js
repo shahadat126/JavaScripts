31258b106a2052491fc902a0a924aa32f7bc16e1
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-29 22:12:10 +0600

    Default & Rest parameter

---
+++ ES6/default_rest_parameter.js
@@ -0,0 +1,15 @@
+//default parameter
+function message(text="default parameter"){
+    console.log(text);
+}
+message();//print default parameter
+message("my name is khan");
+
+
+//rest parameter
+
+function sum(x,y,...z){
+    console.log(`x= ${x},y= ${y},z= ${z}`)
+}
+sum(10,20,30,40,50);
+//...z is rest parameter
