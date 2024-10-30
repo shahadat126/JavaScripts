60fb42ee64d1cd07d1ce2f9d0d67f737bae72c0c
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-30 18:24:03 +0600

    arrow-function part 2

---
+++ ES6/arrow-function2.js
@@ -0,0 +1,28 @@
+let students = [
+  {
+    id: 101,
+    name: "shimul khan",
+    cgpa: 2.92,
+  },
+  {
+    id: 102,
+    name: "sabbir khan",
+    cgpa: 2.72,
+  },
+  {
+    id: 103,
+    name: "sowrob khan",
+    cgpa: 1.72,
+  },
+];
+
+function studentsname() {
+  return students
+    .filter(function (x) {
+      return x.cgpa > 2;
+    })
+    .map(function (y) {
+      return y.name;
+    });
+}
+console.log(studentsname());
