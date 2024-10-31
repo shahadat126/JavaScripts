95566e4a0cf94bd0a4703bccb48e64b3c8e14f65
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-31 21:13:01 +0600

    Function

--- ES6/anonymous-function.js
+++ ES6/anonymous-function.js
@@ -1,4 +1,15 @@
-var message = function () {
-  console.log("this is anonymous function");
-};
-message();
+// var message = function () {
+//   console.log("this is anonymous function");
+// };
+// message();
+
+//passing anonymous function as argument
+
+function display(myfunc) {
+  return myfunc;
+}
+console.log(
+  display(function () {
+    return "argument";
+  })
+);
--- practise/function.js
+++ practise/function.js
@@ -1,9 +1,16 @@
-function Sum(x,y){
-    var addition = x + y;
-    document.write("sum= " +addition);
+// function Sum(x, y) {
+//   var addition = x + y;
+//   document.write("sum= " + addition);
+// }
+
+// var x = parseInt(prompt("enter a number"));
+// var y = parseInt(prompt("enter a number"));
+
+// Sum(x, y);
+
+function display(myfunc) {
+  let a = "khan";
+  let c = myfunc.concat(a);
+  return c;
 }
-
-var x = parseInt(prompt("enter a number"));
-var y = parseInt(prompt("enter a number"));
-
-Sum(x,y);
+console.log(display("shimul"));
