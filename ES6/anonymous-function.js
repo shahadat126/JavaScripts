4c25ad0f8b18e3988455f7b69501ae612ce7a262
shahadat126 <mdshahadathossainzen@gmail.com>
2024-10-31 21:55:58 +0600

    Anonymous Function

--- ES6/anonymous-function.js
+++ ES6/anonymous-function.js
@@ -6,10 +6,21 @@
 //passing anonymous function as argument
 
 function display(myfunc) {
-  return myfunc;
+  return myfunc();
 }
 console.log(
   display(function () {
-    return "argument";
+    let a = "shimul khan";
+    return a;
   })
 );
+
+// returning anonymous function
+function display2(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+let ano = display2(10);
+console.log(ano(20));
